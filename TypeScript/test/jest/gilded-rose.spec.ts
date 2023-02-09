83fda59093dddeb456a9ba3afdf6b46240add0df
Joel Boychuk <joelboychuk@gmail.com>
2023-02-10 00:29:44 +0100

    clean up test suites

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,7 +1,7 @@
 import { Item, GildedRose } from "@/gilded-rose";
 
-type singleTest = (string | number)[];
-const tests: singleTest[] = require("../../test-results.json");
+type singleTestResult = (string | number)[];
+const tests: singleTestResult[] = require("../../test-results.json");
 
 describe("Characterization test suite", () => {
   for (const test of tests) {
@@ -14,7 +14,9 @@ describe("Characterization test suite", () => {
       expect(items[0].quality).toBe(resultQuality);
     });
   }
+});
 
+describe("Tests for Conjure items", () => {
   it("should decrease Conjured items' quality by 2", () => {
     const gildedRose = new GildedRose([new Item("Conjured", 10, 10)]);
     const items = gildedRose.updateQuality();
@@ -29,27 +31,37 @@ describe("Characterization test suite", () => {
   });
 });
 
-// it("should generate all possible test scenarios", () => {
-//   const shopItems: string[] = [
-//     "Aged Brie",
-//     "Backstage passes to a TAFKAL80ETC concert",
-//     "Sulfuras, Hand of Ragnaros",
-//     "other",
-//   ];
-//   const [minSellIn, maxSellIn] = [-1, 12];
-//   const [minQuality, maxQuality] = [-1, 51];
-//   const testResults: singleTest[] = [];
-//   for (const item of shopItems) {
-//     for (let sellIn = minSellIn; sellIn <= maxSellIn; sellIn++) {
-//       for (let quality = minQuality; quality <= maxQuality; quality++) {
-//         const gildedRose = new GildedRose([new Item(item, sellIn, quality)]);
-//         const items = gildedRose.updateQuality();
-//         const resultSellIn = items[0].sellIn;
-//         const resultQuality = items[0].quality;
-//         testResults.push([item, sellIn, quality, resultSellIn, resultQuality]);
+// describe("Generating characterization tests", () => {
+//   it("should generate all possible test scenarios", () => {
+//     const shopItems: string[] = [
+//       "Aged Brie",
+//       "Backstage passes to a TAFKAL80ETC concert",
+//       "Sulfuras, Hand of Ragnaros",
+//       "other",
+//     ];
+//     const [minSellIn, maxSellIn] = [-1, 12];
+//     const [minQuality, maxQuality] = [-1, 51];
+
+//     const testResults: singleTestResult[] = [];
+
+//     for (const item of shopItems) {
+//       for (let sellIn = minSellIn; sellIn <= maxSellIn; sellIn++) {
+//         for (let quality = minQuality; quality <= maxQuality; quality++) {
+//           const gildedRose = new GildedRose([new Item(item, sellIn, quality)]);
+//           const items = gildedRose.updateQuality();
+//           const resultSellIn = items[0].sellIn;
+//           const resultQuality = items[0].quality;
+//           testResults.push([
+//             item,
+//             sellIn,
+//             quality,
+//             resultSellIn,
+//             resultQuality,
+//           ]);
+//         }
 //       }
 //     }
-//   }
-//   console.log(JSON.stringify(testResults));
-//   // pipe test result into .json file
+//     // pipe test result into .json file
+//     console.log(JSON.stringify(testResults));
+//   });
 // });
